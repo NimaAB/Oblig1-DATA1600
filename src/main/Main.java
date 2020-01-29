76cc769cdfbefc0cd4019ffe8c4327cc018ebf44
Jada- <hassan.sheekh@gmail.com>
2020-01-29 14:53:24 +0100

    Kommenter noe- Test

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,12 +14,12 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 500, 600));
         primaryStage.show();
     }
-    public void start1(Stage primaryStage) throws Exception{
+    /*public void start1(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample2.fxml"));
         primaryStage.setTitle("Skjema");
         primaryStage.setScene(new Scene(root, 500, 600));
         primaryStage.show();
-    }
+    }*/
 
     public static void main(String[] args) {
         launch(args);

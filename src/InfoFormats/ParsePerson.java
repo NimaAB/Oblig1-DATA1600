99b8e597d7bc8b210ff1e8ef7d192ebb413e35e0
Glaysa <s344047@oslomet.no>
2020-02-04 13:22:54 +0100

    Merge branch 'master' of https://github.com/NimaAB/Oblig1-DATA1600
     Conflicts:
    	src/main/Controller.java

--- src/InfoFormats/ParsePerson.java
+++ src/InfoFormats/ParsePerson.java
@@ -5,7 +5,7 @@ import avvikshåntering.InvalidPersonFormatException;
 
 public class ParsePerson {
     public static Person parsePerson(String str) throws InvalidPersonFormatException{
-        String [] linjeArray= str.split(PersonFormat.DELIMITER);
+        String [] linjeArray = str.split(PersonFormat.DELIMITER);
         if(linjeArray.length != 5){
             throw new InvalidPersonFormatException("Feil bruk av skiletegn.");
         }

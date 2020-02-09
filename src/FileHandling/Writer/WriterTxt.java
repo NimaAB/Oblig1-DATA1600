bc0b7f946495cc10229e5ef3aff2002c1e1d1aee
NimaAB <nimabewrani98@gmail.com>
2020-02-09 23:35:38 +0100

    en kommentar

--- src/FileHandling/Writer/WriterTxt.java
+++ src/FileHandling/Writer/WriterTxt.java
@@ -10,19 +10,21 @@ import java.io.IOException;
 public class WriterTxt implements WriterInteface {
     public void save(String objString, File filePath, int numberOfLines){
         FileWriter writerParamTo_fWriter;
-        BufferedWriter fWriter = null;
+        BufferedWriter fileWriter = null;
                 try{
                     writerParamTo_fWriter = new FileWriter(filePath);
-                    fWriter= new BufferedWriter(writerParamTo_fWriter);
+                    fileWriter= new BufferedWriter(writerParamTo_fWriter);
                     for(int i=numberOfLines; i> 0;i--){
-                        fWriter.write(objString);
+                        fileWriter.write(objString);
                     }
                 }catch (IOException e){
                     System.err.println(e.getMessage());
                 }
                 try{
-                    assert fWriter != null;//Java føreslo dette har ingen ide.
-                    fWriter.close();
+                    assert fileWriter != null;
+                    //Java føreslo assert, fordi det skal sjekkes om filen
+                    //ikke er null for den lukes.
+                    fileWriter.close();
                 }catch (IOException e){
                     System.err.println(e.getMessage());
                 }
